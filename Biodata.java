94c64af34b64118e61695c21d9bd8cf22b23e1ea
DhabitKhattab <91108865+DhabitKhattab@users.noreply.github.com>
2021-09-29 06:43:27 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,7 @@
+public class Biodata{
+public static void main(String[] args){
+		System.out.println ("Nama : Dhabit Khattab Fanani");
+		System.out.println ("NIM  : 6706210116");
+		System.out.println ("Kelas: D3RPLA45-03");
+	}
+}
